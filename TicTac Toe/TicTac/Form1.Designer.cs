2cbfe7ea77656b49c61f6bca9872ef37de59ffcb
Rafid Shahriar <rss.rafid@gmail.com>
2018-05-21 22:46:03 +0600

    Add files via upload

---
+++ TicTac Toe/TicTac/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace TicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartPanel = new System.Windows.Forms.Panel();
+            this.GamePanel = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
+            this.lblShowName = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.StartPanel.SuspendLayout();
+            this.GamePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StartPanel
+            // 
+            this.StartPanel.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.StartPanel.Controls.Add(this.GamePanel);
+            this.StartPanel.Controls.Add(this.btnStart);
+            this.StartPanel.Controls.Add(this.txtBox2);
+            this.StartPanel.Controls.Add(this.txtBox1);
+            this.StartPanel.Location = new System.Drawing.Point(0, 0);
+            this.StartPanel.Name = "StartPanel";
+            this.StartPanel.Size = new System.Drawing.Size(331, 315);
+            this.StartPanel.TabIndex = 0;
+            // 
+            // GamePanel
+            // 
+            this.GamePanel.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.GamePanel.Controls.Add(this.button10);
+            this.GamePanel.Controls.Add(this.lblShowName);
+            this.GamePanel.Controls.Add(this.button9);
+            this.GamePanel.Controls.Add(this.button8);
+            this.GamePanel.Controls.Add(this.button7);
+            this.GamePanel.Controls.Add(this.button6);
+            this.GamePanel.Controls.Add(this.button5);
+            this.GamePanel.Controls.Add(this.button4);
+            this.GamePanel.Controls.Add(this.button3);
+            this.GamePanel.Controls.Add(this.button2);
+            this.GamePanel.Controls.Add(this.button1);
+            this.GamePanel.Location = new System.Drawing.Point(0, 0);
+            this.GamePanel.Name = "GamePanel";
+            this.GamePanel.Size = new System.Drawing.Size(331, 312);
+            this.GamePanel.TabIndex = 0;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.LightYellow;
+            this.button10.Location = new System.Drawing.Point(16, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(88, 29);
+            this.button10.TabIndex = 0;
+            this.button10.Text = "New Game";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // lblShowName
+            // 
+            this.lblShowName.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowName.Location = new System.Drawing.Point(116, 12);
+            this.lblShowName.Name = "lblShowName";
+            this.lblShowName.Size = new System.Drawing.Size(204, 29);
+            this.lblShowName.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(216, 224);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 68);
+            this.button9.TabIndex = 0;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(116, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 68);
+            this.button8.TabIndex = 0;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(16, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 68);
+            this.button7.TabIndex = 0;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(216, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 68);
+            this.button6.TabIndex = 0;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(116, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 68);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(16, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 68);
+            this.button4.TabIndex = 0;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(216, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 68);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(116, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 68);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 68);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Btn_clicked);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(128, 242);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(82, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox2.ForeColor = System.Drawing.Color.Silver;
+            this.txtBox2.Location = new System.Drawing.Point(97, 169);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(156, 39);
+            this.txtBox2.TabIndex = 0;
+            this.txtBox2.Text = "Player 2";
+            this.txtBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtBox2_MouseClick);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox1.ForeColor = System.Drawing.Color.Silver;
+            this.txtBox1.Location = new System.Drawing.Point(97, 90);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(156, 39);
+            this.txtBox1.TabIndex = 0;
+            this.txtBox1.Text = "Player 1";
+            this.txtBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 312);
+            this.Controls.Add(this.StartPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toe";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.StartPanel.ResumeLayout(false);
+            this.StartPanel.PerformLayout();
+            this.GamePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel StartPanel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Panel GamePanel;
+        private System.Windows.Forms.Label lblShowName;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ TicTac Toe/TicTac/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        private bool player1 = true,player2=false;
+        private List<Button> btnList;
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnList = new List<Button>();
+            this.btnList.Add(this.button1);
+            this.btnList.Add(this.button2);
+            this.btnList.Add(this.button3);
+            this.btnList.Add(this.button6);
+            this.btnList.Add(this.button5);
+            this.btnList.Add(this.button4);
+            this.btnList.Add(this.button9);
+            this.btnList.Add(this.button8);
+            this.btnList.Add(this.button7);
+            this.GamePanel.Hide();
+            this.StartPanel.Show();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+            this.GamePanel.Show();
+            //this.StartPanel.Hide();
+            this.lblShowName.Text = this.txtBox1.Text+" ( X ) ";
+            
+        }
+
+        private void Btn_clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            //CheckPlayer1();
+            //CheckPlayer2();
+            if (player1)
+            {
+                button.Text = "X";
+                button.Enabled = false;
+                button.Focus();
+                button.BackColor = Color.LightYellow;
+                this.player1 = false;
+                this.player2 = true;
+                this.lblShowName.Text = txtBox2.Text + " ( O ) ";
+                if(this.Draw())
+                {
+                    this.lblShowName.Text = "Match Draw";
+                    MessageBox.Show("Match Draw", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (this.CheckPlayer1())
+                {
+                    this.lblShowName.Text = this.txtBox1.Text + " win";
+                    MessageBox.Show(this.txtBox1.Text + " win", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                   
+                }
+                
+            }
+            else if (player2)
+            {
+                button.Text = "O";
+                button.Enabled = false;
+                button.Focus();
+                button.BackColor = Color.LightYellow;
+                this.player2 = false;
+                this.player1 = true;
+                this.lblShowName.Text = txtBox1.Text + " ( X ) ";
+                if (this.Draw())
+                {
+                    this.lblShowName.Text = "Match Draw";
+                    MessageBox.Show("Match Draw", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+                if (this.CheckPlayer2())
+                {
+                    this.lblShowName.Text = this.txtBox2.Text + " win";
+                    MessageBox.Show(this.txtBox2.Text + " win", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.GamePanel.Hide();
+            this.StartPanel.Show();
+            Repeat();
+        }
+
+        private void txtBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.txtBox1.Text = string.Empty;
+            this.txtBox1.ForeColor = Color.Black;
+        }
+
+        private void txtBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.txtBox2.Text = string.Empty;
+            this.txtBox2.ForeColor = Color.Black;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Repeat()
+        {
+            foreach (Button btn in btnList)
+            {
+                btn.Text = string.Empty;
+                btn.BackColor = Color.White;
+                btn.Enabled = true;
+            }
+            player1 = true; player2 = false;
+            //this.lblShowName.Text = this.txtBox1.Text;
+            this.txtBox1.ForeColor = Color.Silver;
+            this.txtBox2.ForeColor = Color.Silver;
+            this.txtBox1.Text = "Player 1";
+            this.txtBox2.Text = "Player 2";
+        }
+
+        private bool CheckPlayer1()
+        {
+            if(button1.Text=="X"&& button2.Text == "X"&& button3.Text == "X") { GetPath( ref button1, ref button2, ref button3); return true; }
+            else if (button4.Text == "X" && button5.Text == "X" && button6.Text == "X") { GetPath(ref button4,ref button5, ref button6); return true; }
+            else if (button7.Text == "X" && button8.Text == "X" && button9.Text == "X") { GetPath(ref button7,ref button8,ref button9); return true; }
+
+            else if (button1.Text == "X" && button5.Text == "X" && button9.Text == "X") { GetPath(ref button1,ref button5,ref button9); return true; }           
+            else if (button3.Text == "X" && button5.Text == "X" && button7.Text == "X") { GetPath(ref button3, ref button5, ref button7); return true; }
+          
+            else if (button1.Text == "X" && button4.Text == "X" && button7.Text == "X") { GetPath(ref button1,ref button4, ref button7); return true; }
+            else if (button2.Text == "X" && button5.Text == "X" && button8.Text == "X") { GetPath(ref button2,ref button5,ref button8); return true; }
+            else if (button3.Text == "X" && button6.Text == "X" && button9.Text == "X") { GetPath(ref button3,ref button6, ref button9); return true; }
+            return false;
+        }
+
+        private bool CheckPlayer2()
+        {
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O") { GetPath(ref button1, ref button2, ref button3); return true; }
+            else if (button4.Text == "O" && button5.Text == "O" && button6.Text == "O") { GetPath(ref button4, ref button5, ref button6); return true; }
+            else if (button7.Text == "O" && button8.Text == "O" && button9.Text == "O") { GetPath(ref button7, ref button8, ref button9); return true; }
+
+            else if (button1.Text == "O" && button5.Text == "O" && button9.Text == "O") { GetPath(ref button1, ref button5, ref button9); return true; }
+            else if (button3.Text == "O" && button5.Text == "O" && button7.Text == "O") { GetPath(ref button3, ref button5, ref button7); return true; }
+
+            else if (button1.Text == "O" && button4.Text == "O" && button7.Text == "O") { GetPath(ref button1, ref button4, ref button7); return true; }
+            else if (button2.Text == "O" && button5.Text == "O" && button8.Text == "O") { GetPath(ref button2, ref button5, ref button8); return true; }
+            else if (button3.Text == "O" && button6.Text == "O" && button9.Text == "O") { GetPath(ref button3, ref button6, ref button9); return true; }
+            return false;
+        }
+        private void GetPath( ref Button button1,ref Button button2,ref Button button3 )
+        {
+            button1.BackColor = Color.DarkRed;
+            button2.BackColor = Color.DarkRed;
+            button3.BackColor = Color.DarkRed;
+            foreach (Button item in btnList)
+            {
+                item.Enabled = false;
+            }
+        }
+
+        private bool Draw()
+        {
+            foreach (Button item in btnList)
+            {
+                if (item.Enabled) { return false; }
+            }
+            return true;
+        }
+    }
+}
